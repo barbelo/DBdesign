b2948dd949f8b28f6641e446e5410a6e537441ed
shirley <18281262@bjtu.edu.cn>
2020-12-25 02:03:07 +0800

    add new airline

--- routes/insert-airline.js
+++ routes/insert-airline.js
@@ -16,51 +16,43 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res, next) {
     // console.log(req.body);
 
-    let cmpy_id = req.body.companyname;//获取前台请求的参数
-    let password = req.body.password;
+    let companyname = req.body.companyname;
+    let airline_id = req.body.airline_id;
+    let departure_T = req.body.departure_T;
+    let arrival_T = req.body.arrival_T;
+    let departure_airport = req.body.departure_airport;
+    let arrival_airport = req.body.arrival_airport;
 
     pool.getConnection(function (err, connection) {
-        //先判断该账号是否存在
-        let $sql = "select * from air_company where cmpy_id = ?";
 
-        connection.query($sql, [cmpy_id], function (err, result) {
+        //判断航班是否已经存在
+        let $sql = "select * from airline where airline_id = ?";
+
+        connection.query($sql, [airline_id], function (err, result) {
             let resultJson = result;
 
             if (resultJson.length === 0) {
+                let $sql1 = "insert into airline VALUES(?, ?, ?, ?, ?, null, ?)";
+                connection.query($sql1, [airline_id, departure_T,
+                    arrival_T, departure_airport, arrival_airport, companyname], function (err, result) {
 
-                result = {
-                    code: 300,
-                    msg: '该账号不存在'
-                };
-                res.json(result);
-                connection.release();
+                    // console.log(result);
+                    result = {
+                        code: 200,
+                        msg: 'correct insert'
+                    };
 
-            } else {
-                //账号存在，可以登录，进行密码判断
-
-                let $sql1 = "select cmpy_id, password from air_company where cmpy_id = ?";
-
-                connection.query($sql1, [cmpy_id], function (err, result) {
-                    let temp = result[0].password;  //取得数据库查询字段值
-                    // console.log(temp);
-
-                    if (temp === password) {
-                        //获取uid
-                        result = {
-                            code: 200,
-                            msg: '密码正确',
-                            uid: result[0].psg_id
-                        };
-                    } else {
-                        result = {
-                            code: 400,
-                            msg: '密码错误'
-                        };
-
-                    }
                     res.json(result); // 以json形式，把操作结果返回给前台页面
                     connection.release();// 释放连接
                 });
+
+            } else {
+                result = {
+                    code: 300,
+                    msg: '已经存在此航班号'
+                };
+                res.json(result);
+                connection.release();
             }
         });
     });
